1d5b0bf90c635d96b6c69181839fdadb38047be2
Elizeu <el_vailant@hotmail.com>
2023-03-23 11:39:21 -0400

    teste tutorial

--- say_hello.py
+++ say_hello.py
@@ -9,11 +9,51 @@ class SayHello(App):
     def build(self):
         self.window = GridLayout()
         self.window.cols = 1
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+        
+        
+        
         
         #add widgets to window
+        # image widget
         self.window.add_widget(Image(source="robo pensador.jpg"))
-
+        
+        # label widget
+        self.greeting = Label(
+                            text="Qual o seu nome?",
+                            font_size = 18,
+                            color="00FFEE"
+                        )
+        self.window.add_widget(self.greeting)
+        
+        # text input widget
+        self.user = TextInput(
+                        multiline=False,
+                        padding_y = (20,20),
+                        size_hint = (1, 0.5)                    
+                    )
+        self.window.add_widget(self.user)
+        
+        # button widget
+        self.button = Button(
+                            text="GREET",
+                            size_hint = (1, 0.5),
+                            bold = True,
+                            background_color = "#00FFCE"
+                        )
+        self.button.bind(on_press=self.callback)
+        self.window.add_widget(self.button)
+        
         return self.window
+        
+    def callback(self, instance):
+        self.greeting.text = "Hello " + self.user.text + "!"
+        
+        
+        
+        
+        
 
 if __name__ == "__main__":
     SayHello().run()
