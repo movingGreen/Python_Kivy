cdbe4b36ec8395ac4345bdcdc01459390ed07111
Elizeu <el_vailant@hotmail.com>
2023-03-23 15:42:01 -0400

    testando apresentar uma imagem

--- calculadora_tutorial/hello_world.py
+++ calculadora_tutorial/hello_world.py
@@ -1,14 +1,14 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.image import Image
 
 class MainApp(App):
   def build(self):
-    label = Label(text='Hello from Kivy',
-                  size_hint=(.5, .5),
-                  pos_hint={'center_x': .5, 'center_y': .5}
+    image = Image(source = 'robo pensador.jpg',
+                  size_hint = (1, .5),
+                  pos_hint = {'center_x': .5, 'center_y': .5}
                   )
-    return label
+    return image
   
-if __name__ = '__main__':
+if __name__ == '__main__':
   app = MainApp()
   app.run()
