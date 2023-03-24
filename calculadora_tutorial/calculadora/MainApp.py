b9dd86aeb4296fe33d7cd9e1112d7a09f33dec7d
Elizeu <el_vailant@hotmail.com>
2023-03-24 11:34:38 -0400

    código dos botões e do enter

--- calculadora_tutorial/calculadora/MainApp.py
+++ calculadora_tutorial/calculadora/MainApp.py
@@ -46,9 +46,40 @@ class MainApp(App):
                 "center_y": 0.5
             }
         )
+        
         equals_button.bind(on_press = self.on_solution)
         main_layout.add_widget(equals_button)
         
         return main_layout
             
+    def on_button_press(self, instance):
+        current = self.solution.text
+        button_text = instance.text
+        
+        if button_text == "C":
+            # Clear the solution widget
+            self.solution.text = ""
             
+        else:
+            if current and (self.last_was_operator and button_text in self.operators):
+                # Don't add two operators right after each other
+                return
+            
+            elif current == "" and button_text in self.operators:
+                # First character cannot be an operator
+                return
+            
+            else:
+                new_text = current + button_text
+                self.solution.text = new_text
+                
+        self.last_button = button_text
+        self.last_was_operator = self.last_button in self.operators
+    
+    def on_solution(self, instance):
+        text = self.solution.text
+        
+        if text:
+            solution = str(eval(self.solution.text))
+            self.solution.text = solution
+                
