11b905348b9cb9b81e439a10aa8d9a83ddf5540d
Elizeu <el_vailant@hotmail.com>
2023-03-29 12:16:33 -0400

    tentando mostrar o mapa na localização do usuario desktop

--- teste_geolocalização/mapView/main.py
+++ teste_geolocalização/mapView/main.py
@@ -6,7 +6,10 @@ class MainApp(App):
     longitude = -25
     
     def on_start(self):
-        marker = MapMarkerPopup(lat = self.latitude, lon = )
+        marker = MapMarkerPopup(lat = self.latitude, lon = self.longitude)
+        self.root.lat = self.latitude
+        self.root.lon = self.longitude
+        self.root.add_widget(marker)
     pass
 
 MainApp().run()
