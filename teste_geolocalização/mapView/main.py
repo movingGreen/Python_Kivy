7dba3d614b74ac1d650bc07cfcadc94eb4b759c2
Elizeu <el_vailant@hotmail.com>
2023-03-29 12:21:20 -0400

    continuando tentando

--- teste_geolocalização/mapView/main.py
+++ teste_geolocalização/mapView/main.py
@@ -2,14 +2,16 @@ from kivy.app import App
 from kivy.garden.mapview import MapMarkerPopup
 
 class MainApp(App):
-    latitude = -25
-    longitude = -25
+   
     
     def on_start(self):
-        marker = MapMarkerPopup(lat = self.latitude, lon = self.longitude)
-        self.root.lat = self.latitude
-        self.root.lon = self.longitude
+        latitude = -25
+        longitude = -25
+        marker = MapMarkerPopup(lat = latitude, lon = longitude)
+        # self.root.lat = latitude
+        # self.root.lon = longitude
         self.root.add_widget(marker)
-    pass
+        pass
+    
 
 MainApp().run()
