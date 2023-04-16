d7817a2c756da8cd5efd949c05f3da85d68b2fa9
Elizeu <el_vailant@hotmail.com>
2023-04-16 17:37:06 -0400

    função no .py funcionando no .kv

--- tutorial python GUI/lang_kv/teste_lang_kv.py
+++ tutorial python GUI/lang_kv/teste_lang_kv.py
@@ -5,43 +5,30 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 class MyGridLayout(Widget):
-    # inicializar a palavra chave infinite
-    def __init__(self, **kwargs):
-        # Chamar o construtor do grid layout
-        super(MyGridLayout, self).__init__(**kwargs)
-
-        # crie as colunas
-        self.cols = 1
-        self.row_force_default = True
-        self.col_force_default = True
-        self.row_default_height = 120
-        self.col_default_width = 400
-        
-        # criar um segundo gridlayout
-        self.top_grid = GridLayout(
-            row_force_default = True,
-            row_default_height = 40,
-            col_force_default = True,
-            col_default_width = 400
-        )
-     
+    
+    name = ObjectProperty(None)
+    senha = ObjectProperty(None)
+    
     # método press
-    def press(self, instance):
+    def press(self):
         username = self.name.text
         password = self.senha.text
         
-        if username != "" and not username.isspace():
-            print(f'Usuario: {username}, Senha: {password}')
-            self.add_widget(Label(text = f"Olá {username}"))
-        else: 
-            print("Usuario vazio")
+        # if username != "" and not username.isspace():
+        #     print(f'Usuario: {username}, Senha: {password}')
+        #     self.add_widget(Label(text = f"Olá {username}"))
+        # else: 
+        #     print("Usuario vazio")
             
+        print(f"O seu nome {username}, a senha {password}")
+
         #limpar o texto dos campos de input
         self.name.text = ""
         self.senha.text = ""
-
+        
 class MyApp(App):
     def build(self):
         return MyGridLayout()
